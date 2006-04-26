0234f765ab71ded19a270f9685a13105f1496d51
Hisham Muhammad <hisham@gobolinux.org>
2006-04-26 13:50:35 +0000

    Support calling functions from other VMs through script_call.

--- libscript-lua/src/libscript-lua.c
+++ libscript-lua/src/libscript-lua.c
@@ -7,7 +7,7 @@
 
 #include "libscript-lua.h"
 
-static script_env* script_lua_get_env(lua_State *L) {
+INLINE static script_env* script_lua_get_env(lua_State *L) {
    script_env* env;
    lua_pushlightuserdata(L, L);
    lua_gettable(L, LUA_REGISTRYINDEX);
@@ -16,9 +16,7 @@ static script_env* script_lua_get_env(lua_State *L) {
    return env;
 }
 
-static int script_lua_call(lua_State *L) {
-   script_env* env = script_lua_get_env(L);
-   script_fn fn;
+INLINE static void script_lua_get_params(script_env* env, lua_State *L) {
    int nargs;
    int i;
    script_start_params(env);
@@ -30,34 +28,45 @@ static int script_lua_call(lua_State *L) {
       /* TODO: other types */
       }
    }
-   fn = (script_fn) lua_touserdata(L, lua_upvalueindex(1));
-   script_reset_outs(env);
-   fn(env);
-   /* TODO: return values */
-   return 0;
+}
+
+INLINE static int script_lua_put_params(script_env* env, lua_State *L) {
+   script_type type;
+   int nargs;
+   nargs = 0;
+   while ( (type = script_in_type(env)) != SCRIPT_NONE ) {
+      nargs++;
+      switch (type) {
+      case SCRIPT_DOUBLE: lua_pushnumber(L, script_in_double(env)); break;
+      case SCRIPT_STRING: lua_pushstring(L, script_in_string(env)); break;
+      case SCRIPT_NONE: /* pacify gcc warning */ break;
+      /* TODO: other types */
+      }
+   }
+   return nargs;
+}
+
+static int script_lua_call(lua_State *L) {
+   script_env* env = script_lua_get_env(L);
+   const char* name;
+   script_err err;
+   script_lua_get_params(env, L);
+   name = lua_tostring(L, lua_upvalueindex(1));
+   err = script_call(env, name);
+   if (err != SCRIPT_OK) {
+      lua_pushstring(L, "No such function.");
+      lua_error(L);
+   }
+   return script_lua_put_params(env, L);
 }
 
 static int script_lua_find_function(lua_State *L) {
-   script_env* env = script_lua_get_env(L);
-   const char* name;
-   script_fn fn;
-
-   name = lua_tostring(L, 2);
-
-   fn = script_get_function(env, name);
-   if (!fn) {
-      lua_settop(L, 0);
-      lua_pushnil(L);
-      return 1;
-   }
-   lua_pushlightuserdata(L, fn);
    lua_pushcclosure(L, script_lua_call, 1);
    lua_pushvalue(L, 1);
    lua_pushvalue(L, 2);
    lua_pushvalue(L, -3);
    lua_settable(L, -3);
    lua_pop(L, 1);
-
    return 1;
 }
 
@@ -87,7 +96,7 @@ script_plugin_state script_plugin_init_lua(script_env* env) {
    return (script_plugin_state) L;
 }
 
-int script_plugin_run_lua(script_plugin_state state, char* programtext) {
+script_err script_plugin_run_lua(script_plugin_state state, char* programtext) {
    lua_State* L = (lua_State*) state;
    int err;
 
@@ -107,35 +116,24 @@ script_err script_plugin_call_lua(script_plugin_state state, char* fn) {
    lua_State* L = (lua_State*) state;
    script_env* env;
    const char* namespace;
-   script_type type;
    int args;
    int err;
 
-   lua_pushlightuserdata(L, L);
-   lua_gettable(L, LUA_REGISTRYINDEX);
-   env = (script_env*) lua_touserdata(L, -1);
+   env = script_lua_get_env(L);
    namespace = script_get_namespace(env);
 
    lua_getfield(L, LUA_GLOBALSINDEX, namespace);
    lua_pushstring(L, fn);
    lua_gettable(L, -2);
-   if (!lua_isfunction(L, -1))
+   if (!lua_isfunction(L, -1) || lua_iscfunction(L, -1))
       return SCRIPT_ERRFNUNDEF;
-   args = 0;
-   while ( (type = script_in_type(env)) != SCRIPT_NONE ) {
-      args++;
-      switch (type) {
-      case SCRIPT_DOUBLE: lua_pushnumber(L, script_in_double(env)); break;
-      case SCRIPT_STRING: lua_pushstring(L, script_in_string(env)); break;
-      case SCRIPT_NONE: /* pacify gcc warning */ break;
-      }
-   }
-   /* TODO: returns */
+   args = script_lua_put_params(env, L);
    err = lua_pcall(L, args, 0, 0);
    if (err) {
       script_set_error_message(env, lua_tostring(L, -1));
       return SCRIPT_ERRLANGRUN;
    }
+   script_lua_get_params(env, L);
    return SCRIPT_OK;
 }
 
--- libscript-lua/src/libscript-lua.h
+++ libscript-lua/src/libscript-lua.h
@@ -2,8 +2,15 @@
 #ifndef LIBSCRIPT_LUA_H
 #define LIBSCRIPT_LUA_H
 
+#ifdef __GNUC__
+#define INLINE __inline
+#else
+#define INLINE
+#endif
+
 script_plugin_state script_plugin_init_lua(script_env* env);
-int script_plugin_run_lua(script_plugin_state state, char* programtext);
+script_err script_plugin_run_lua(script_plugin_state state, char* programtext);
+script_err script_plugin_call_lua(script_plugin_state state, char* fn);
 void script_plugin_done_lua(script_plugin_state state);
 
 #endif
