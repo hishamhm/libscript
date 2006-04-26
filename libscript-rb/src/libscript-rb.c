e2c45c257b406c1984419e4c1366b7a11c21c5a3
Hisham Muhammad <hisham@gobolinux.org>
2006-04-26 13:49:46 +0000

    Make fetching of functions dynamic; Support return values;

--- libscript-rb/src/libscript-rb.c
+++ libscript-rb/src/libscript-rb.c
@@ -2,12 +2,19 @@
 #include <libscript-plugin.h>
 
 /* Ouch! Ruby's headers are not ANSI C! */
-#define inline 
+#ifdef __GNUC__
+#define INLINE __inline
+#define inline __inline
+#else
+#define INLINE
+#define inline
+#endif
 
 /* Since we're not ANSI C anyway, use snprintf */
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <ctype.h>
+#include <assert.h>
 
 #include <ruby.h>
 
@@ -19,47 +26,92 @@ static script_env* script_rb_env;
 static char* script_rb_namespace;
 static ID method_id;
 
-static VALUE script_rb_call(VALUE self, VALUE fn_value, VALUE args) {
-   script_fn fn;
-   VALUE arg;
-
-   script_start_params(script_rb_env);
-   
-   fn = (script_fn) NUM2LONG(fn_value);
-   while ( (arg = rb_ary_shift(args)) != Qnil ) {
-      switch (TYPE(arg)) {
-      case T_FLOAT:
-      case T_FIXNUM:
-      case T_BIGNUM:
-         script_out_double(script_rb_env, NUM2DBL(arg));
-         break;
-      case T_STRING:
-         script_out_string(script_rb_env, STR2CSTR(arg));
-         break;
-      default:;
-         /* TODO: other types */
-      }
+INLINE static void script_rb_get_param(script_env* env, VALUE arg) {
+   switch (TYPE(arg)) {
+   case T_FLOAT:
+   case T_FIXNUM:
+   case T_BIGNUM:
+      script_out_double(env, NUM2DBL(arg));
+      break;
+   case T_STRING:
+      script_out_string(env, StringValuePtr(arg));
+      break;
+   default:;
+      /* TODO: other types */
    }
-   script_reset_outs(script_rb_env);
-
-   fn(script_rb_env);
-
-   return Qnil;
 }
 
-static VALUE script_rb_get(VALUE klass, VALUE name_value) {
+INLINE static void script_rb_get_params(script_env* env, VALUE args) {
+   VALUE arg;
+
+   script_start_params(env);
+   while ( (arg = rb_ary_shift(args)) != Qnil ) {
+      script_rb_get_param(env, arg);
+   }
+}
+
+INLINE static VALUE* script_rb_put_params(script_env* env, int params) {
+   int i;
+   VALUE* args = malloc(sizeof(VALUE) * params);
+   for (i = 0; i < params; i++) {
+      VALUE arg = (VALUE) NULL;
+      switch (script_in_type(env)) {
+      case SCRIPT_DOUBLE:
+         arg = rb_float_new(script_in_double(env)); 
+         break;
+      case SCRIPT_STRING:
+         arg = rb_str_new2(script_in_string(env)); 
+         break;
+      default:
+         /* pacify gcc warnings */
+         assert(0);
+      }
+      assert(arg);
+      args[i] = arg;
+   }
+   return args;
+}
+
+static VALUE script_rb_call(VALUE self, VALUE fn_value, VALUE args) {
+   script_fn fn;
+   VALUE* rets;
+   int nargs;
+   VALUE ret;
+
+   fn = (script_fn) NUM2LONG(fn_value);
+   script_rb_get_params(script_rb_env, args);
+   fn(script_rb_env);
+   nargs = script_param_count(script_rb_env);
+   if (nargs > 0) {
+      rets = script_rb_put_params(script_rb_env, nargs);
+      if (nargs == 1)
+         ret = rets[0];
+      else
+         ret = rb_ary_new4(nargs, rets);
+      free(rets);
+   } else
+      ret = Qnil;
+   return ret;
+}
+
+static VALUE script_rb_method_missing(int argc, VALUE* argv, VALUE obj) {
    char* name;
    script_fn fn;
-   VALUE method;
-   
-   name = STR2CSTR(name_value);
+
+   name = rb_id2name(SYM2ID(argv[0]));
    fn = script_get_function(script_rb_env, name);
+   
    if (fn) {
       char fn_code[1024];
-      snprintf(fn_code, 1023, "def %s.%s(*args); %s.call(%ld, args); end;", script_rb_namespace, name, script_rb_namespace, (long int) fn);
+      VALUE args;
+      snprintf(fn_code, 1023, "def %s.%s(*args); %s.call(%ld, args); end;",
+         script_rb_namespace, name, script_rb_namespace, (long int) fn);
       rb_eval_string(fn_code);
-      method = rb_funcall(script_rb_class, method_id, 1, name_value);
-      return method;
+      args = rb_ary_new4(argc - 1, argv+1);
+      return script_rb_call(obj, rb_int_new((long int) fn), args);
+   } else {
+      /* FIXME: I'm getting a Ruby segfault when a function is not found. */
+      rb_raise(rb_eRuntimeError, "No such function: '%s'.", name);
    }
    return Qnil;
 }
@@ -78,7 +130,7 @@ script_plugin_state script_plugin_init_rb(script_env* env) {
    script_rb_namespace[0] = toupper(script_rb_namespace[0]);
    script_rb_class = rb_define_class(script_rb_namespace, rb_cObject);
    method_id = rb_intern("method");
-   rb_define_singleton_method(script_rb_class, "get", script_rb_get, 1);
+   rb_define_singleton_method(script_rb_class, "method_missing", script_rb_method_missing, -1);
    rb_define_singleton_method(script_rb_class, "call", script_rb_call, 2);
    return SCRIPT_GLOBAL_STATE;
 }
@@ -89,9 +141,25 @@ int script_plugin_run_rb(script_plugin_state state, char* programtext) {
 }
 
 int script_plugin_call_rb(script_plugin_state state, char* fn) {
-   /* TODO */
+   VALUE method;
+   ID fn_id;
+   VALUE fn_value;
+   int params;
+   VALUE* args;
+   VALUE ret;
+
+   fn_value = rb_str_new2(fn);
+   method = rb_funcall(script_rb_class, method_id, 1, fn_value);
+   if (method == Qnil)
+      return SCRIPT_ERRFNUNDEF;
+   fn_id = rb_intern(fn);
+   params = script_param_count(script_rb_env);
+   args = script_rb_put_params(script_rb_env, params); 
+   ret = rb_funcall2(script_rb_class, fn_id, params, args);
+   script_rb_get_param(script_rb_env, ret);
    return SCRIPT_OK;
 }
 
 void script_plugin_done_rb(script_plugin_state state) {
+   /* TODO: at least reset the environment, if possible */
 }
