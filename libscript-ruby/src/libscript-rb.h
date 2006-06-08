11834858e63beeb7f0f1c830a18c05300fe6ed09
Hisham Muhammad <hisham@gobolinux.org>
2006-06-08 00:56:04 +0000

    Will change to libscript-ruby.h within seconds

--- libscript-ruby/src/libscript-rb.h
+++ libscript-ruby/src/libscript-rb.h
@@ -1,10 +1,10 @@
 
-#ifndef LIBSCRIPT_PY_H
-#define LIBSCRIPT_PY_H
+#ifndef LIBSCRIPT_RUBY_H
+#define LIBSCRIPT_RUBY_H
 
-script_plugin_state script_plugin_init_rb(script_env* env);
-int script_plugin_run_rb(script_plugin_state state, char* programtext);
-int script_plugin_call_rb(script_plugin_state state, char* fn);
-void script_plugin_done_rb(script_plugin_state state);
+script_plugin_state script_plugin_init_ruby(script_env* env);
+int script_plugin_run_ruby(script_plugin_state state, char* programtext);
+int script_plugin_call_ruby(script_plugin_state state, char* fn);
+void script_plugin_done_ruby(script_plugin_state state);
 
 #endif
