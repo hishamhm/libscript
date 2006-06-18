13a4b9014187b41997fe85f0212bc61e517658fd
Hisham Muhammad <hisham@gobolinux.org>
2006-06-18 23:42:29 +0000

    New Java example for GC section.

--- tests_lang/javatestjni.c
+++ tests_lang/javatestjni.c
@@ -3,12 +3,40 @@
 #include <stdio.h>
 #include "javatestjni.h"
 
-JNIEXPORT void JNICALL Java_javatestjni_myNativeMethod(JNIEnv *env, jobject this) {
-   jclass klass = (*env)->GetObjectClass(env, this);
-   jfieldID wordsID = (*env)->GetFieldID(env, klass, "words", "[Ljava/lang/String;");
-   jarray words = (*env)->GetObjectField(env, this, wordsID);
-   jstring words1 = (*env)->GetObjectArrayElement(env, words, 1);
-   const char* words1str = (*env)->GetStringUTFChars(env, words1, NULL);
+JNIEXPORT void JNICALL Java_javatestjni_myNativeMethod(JNIEnv *J, jobject this) {
+   jclass klass = (*J)->GetObjectClass(J, this);
+   jfieldID wordsID = (*J)->GetFieldID(J, klass, "words", "[Ljava/lang/String;");
+   jarray words = (*J)->GetObjectField(J, this, wordsID);
+   jstring words1 = (*J)->GetObjectArrayElement(J, words, 1);
+   const char* words1str = (*J)->GetStringUTFChars(J, words1, NULL);
    printf("%s\n", words1str);
-   (*env)->ReleaseStringUTFChars(env, words1, words1str);
+   (*J)->ReleaseStringUTFChars(J, words1, words1str);
+}
+
+JNIEXPORT jstring JNICALL Java_javatestjni_concatArray(JNIEnv *J, jobject this, jobjectArray t) {
+   static jmethodID concat = NULL, toString = NULL;
+   int i;
+   if (!concat) {
+      jclass cls = (*J)->FindClass(J, "java/lang/String");
+      concat = (*J)->GetMethodID(J, cls, "concat",
+         "(Ljava/lang/String;)Ljava/lang/String;");
+      cls = (*J)->FindClass(J, "java/lang/Object");
+      toString = (*J)->GetMethodID(J, cls, "toString",
+         "()Ljava/lang/String;");
+   }
+   jstring s = (*J)->NewString(J, NULL, 0);
+   s = (*J)->NewGlobalRef(J, (jobject) s);
+   int len = (*J)->GetArrayLength(J, t);
+   for (i = 0; i < len; i++) {
+      (*J)->PushLocalFrame(J, 10);
+      jobject r = (*J)->GetObjectArrayElement(J, t, i);
+      jstring str = (*J)->CallObjectMethod(J, r, toString);
+      str = (*J)->CallObjectMethod(J, s, concat, str);
+      (*J)->DeleteGlobalRef(J, s);
+      s = (*J)->NewGlobalRef(J, str);
+      (*J)->PopLocalFrame(J, NULL);
+   }
+   jobject ret = (*J)->NewLocalRef(J, s);
+   (*J)->DeleteGlobalRef(J, s);
+   return ret;
 }
--- tests_lang/javatestjni.java
+++ tests_lang/javatestjni.java
@@ -1,9 +1,14 @@
 
 public class javatestjni {
    private String[] words = { "Earth", "Wind", "Fire", "Water" };
+   private Double[] numbers = { 1.0, 2.0, 1.2, 2.12, 12.212, 2121.2212 };
    private native void myNativeMethod();
+   private native String concatArray(Object[] nums);
    public static void main(String[] args) {
-      new javatestjni().myNativeMethod();
+      javatestjni j = new javatestjni();
+      j.myNativeMethod();
+      System.out.println("The sum is: " + j.concatArray(j.words));
+      System.out.println("The sum is: " + j.concatArray(j.numbers));
    }
    static {
       System.loadLibrary("javatestjni");
