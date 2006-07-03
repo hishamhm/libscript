c1f8af12aaa2b41053204cdae15fa45770b662ae
Hisham Muhammad <hisham@gobolinux.org>
2006-07-03 02:11:42 +0000

    Improve rubycall. Used it in an example in docs.

--- tests_lang/rubycall.c
+++ tests_lang/rubycall.c
@@ -8,11 +8,12 @@ VALUE a_C_block() {
 }
 
 VALUE call_a_ruby_function() {
-   rb_funcall(Qnil, a_ruby_function_ID, 0);
+   return rb_funcall(Qnil, a_ruby_function_ID, 0);
 }
 
 int main(int argc, char** argv) {
    VALUE a_proc;
+   VALUE result;
 
    ruby_init();
    
@@ -54,14 +55,31 @@ int main(int argc, char** argv) {
 
    rb_eval_string(
    "   def a_ruby_function() \n"
-   "      print(\"a_ruby_function will yield.\\n\") \n"
+   "      print(\"\\na_ruby_function will yield.\\n\") \n"
    "      yield \n"
    "      print(\"a_ruby_function is done.\n\") \n"
+   "      return 42 \n"
    "   end \n"
    );
 
-   rb_iterate(call_a_ruby_function, Qnil, a_C_block, Qnil);
+   result = rb_iterate(call_a_ruby_function, Qnil, a_C_block, Qnil);
+   fprintf(stderr, "Result = %d\n", NUM2INT(result));
 
+   /* Or this... */
+   
+   a_proc = rb_proc_new(a_C_block, Qnil);
+   rb_gv_set("$a_proc", a_proc);
+   rb_eval_string("$result = a_ruby_function(&$a_proc)");
+   rb_gv_get("$result");
+   fprintf(stderr, "Result = %d\n", NUM2INT(result));
+
+   /* Or this... */
+   
+   rb_define_global_function("a_C_block", a_C_block, 0);
+   rb_eval_string("$result = a_ruby_function { a_C_block }");
+   rb_gv_get("$result");
+   fprintf(stderr, "Result = %d\n", NUM2INT(result));
+   
    ruby_finalize();
    
    return 0;
